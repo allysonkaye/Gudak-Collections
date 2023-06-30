294314646ee7046abc9d1f5066df9e8239eecded
allysonkaye <allysonkayetimpug06@gmail.com>
2023-06-30 18:04:21 +0800

    Redesign

--- gallery.js
+++ gallery.js
@@ -8,17 +8,20 @@ function openNav() {
 
 
 const colorSwitch = document.getElementById("color-switch");
+const off = document.getElementById("off");
+
 
 colorSwitch.addEventListener('click',checkMode);
 function checkMode(){
     if(colorSwitch.checked) {
         console.log ("darkmode");
         darkMode();
+        off.innerHTML = "On";
     }
     else {
         console.log ("lightmode");
         lightMode();
-
+        off.innerHTML = "Off";
 
     }
 }
